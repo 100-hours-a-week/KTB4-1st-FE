3fffb03fafbbc8b548cd298c93b379f3cb3d19fa
Stella <yeobinlee1210@gmail.com>
2026-09-17 23:03:29 +0900

    chore: 프론트엔드 127.0.0.1 접속 허용

--- ktb_agile_frontend/next.config.ts
+++ ktb_agile_frontend/next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   devIndicators: false,
   output: 'standalone',
+  allowedDevOrigins: ['127.0.0.1'],
+
 }
 
 export default nextConfig
