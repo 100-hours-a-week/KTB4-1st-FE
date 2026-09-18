bce687913abb0a64015630ec24ae158a618e15bc
Stella <yeobinlee1210@gmail.com>
2026-09-18 13:46:08 +0900

    feat(navbar): 네비바 페이지 라우팅 기능 구현

---
+++ ktb_agile_frontend/src/app/pages/groups/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/common/navbar/Navbar"
+
+export default function GroupList(){
+    return(
+        <div>
+            group list
+            <Navbar/>
+        </div>
+    )
+}
