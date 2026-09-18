8779d08dd32de00854ec432d6457c3fe15027e3c
Stella <yeobinlee1210@gmail.com>
2026-09-18 13:34:14 +0900

    feat(itemList): 물품 페이지 목록 생성

--- ktb_agile_frontend/src/app/pages/items/page.tsx
+++ ktb_agile_frontend/src/app/pages/items/page.tsx
@@ -1,7 +1,11 @@
-export default function ItemList(){
-    return(
-        <div>
-            item list
-        </div>
-    )
+import Navbar from '@/components/common/navbar/Navbar'
+import styles from './page.module.css'
+
+export default function ItemList() {
+  return (
+    <section className={styles.page}>
+      <div>item list</div>
+      <Navbar />
+    </section>
+  )
 }
