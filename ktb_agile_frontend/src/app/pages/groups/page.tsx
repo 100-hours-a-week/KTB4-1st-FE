523a19fc617f221e72a76117383b396fdf7863a4
Stella <yeobinlee1210@gmail.com>
2026-09-20 21:15:50 +0900

    feat(group): 그룹 목록 페이지 생성
    - 그룹 검색창 및 검색 버튼 생성
    - 그룹 생성 버튼 생성

--- ktb_agile_frontend/src/app/pages/groups/page.tsx
+++ ktb_agile_frontend/src/app/pages/groups/page.tsx
@@ -1,10 +1,38 @@
-import Navbar from "@/components/common/navbar/Navbar"
+'use client'
+import Navbar from '@/components/common/navbar/Navbar'
+import styles from './page.module.css'
+import { useRouter } from 'next/navigation'
 
-export default function GroupList(){
-    return(
-        <div>
-            group list
-            <Navbar/>
-        </div>
-    )
+export default function GroupList() {
+    const router = useRouter()
+
+  return (
+    <section className={styles.page}>
+      <header className={styles.header}>
+        <button className={styles.backButton} onClick={()=>router.back()}>
+            {/* 뒤로가기 아이콘 */}
+          <svg viewBox="0 0 24 24">
+            <path d="m14.5 5.5-6.5 6.5 6.5 6.5M8 12h12" />
+          </svg>
+        </button>
+        <h1 className={styles.title}>그룹</h1>
+        <button className={styles.createButton} type="button" onClick={()=>router.push('/pages/groups/create')}>
+          그룹 생성하기
+        </button>
+      </header>
+
+      <div className={styles.search}>
+        <input
+          className={styles.searchInput}
+          type="search"
+          placeholder="그룹명을 검색"
+        />
+        <button className={styles.searchButton} type="button">
+          그룹검색
+        </button>
+      </div>
+
+      <Navbar />
+    </section>
+  )
 }
